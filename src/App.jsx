676c05a0a10f8a5190a62c9fe73b217b20d234f5
Nikhil Gupta <jerrycodes29@gmail.com>
2023-05-04 23:53:15 +0530

    add newsletter section comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import AboutUs from "./components/AboutUs"
 import DeliveryService from "./components/DeliveryService"
 import Hero from "./components/Hero"
+import Newsletter from "./components/Newsletter"
 import PopularNow from "./components/PopularNow"
 import SpecialMenu from "./components/SpecialMenu"
 
@@ -12,6 +13,7 @@ export default function App() {
       <DeliveryService />
       <AboutUs />
       <SpecialMenu />
+      <Newsletter />
       <div className='min-h-screen'></div>
     </>
   )
---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,28 @@
+import Button from "./utils/Button"
+import Container from "./utils/Container"
+
+export default function Newsletter() {
+  return (
+    <Container>
+      <div className='h-[400px] w-full rounded-xl bg-[url("https://images.unsplash.com/photo-1551887196-72e32bfc7bf3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1029&q=80")] bg-cover bg-center bg-no-repeat'>
+        <div className='flex h-full flex-col items-center justify-center space-y-5 p-5 text-neutral-50'>
+          <h3 className='text-center text-2xl lg:text-3xl'>
+            Subscribe to our Newsletter & Get 30% discount
+          </h3>
+          <div className='relative w-full lg:w-[500px]'>
+            <input
+              type='email'
+              className='block w-full rounded-full border border-gray-300 bg-gray-50 p-4 text-sm text-gray-900 '
+              placeholder='Email Address'
+              required
+            />
+
+            <div className='absolute bottom-0.5 right-1'>
+              <Button text={"Order Now"} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  )
+}
